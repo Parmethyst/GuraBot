a358c4a03415ae321a30d02c097493796e925e58
vsky84 <vsky84@hotmail.com>
2020-09-26 17:55:04 +0700

    Added loggings

--- gura.js
+++ gura.js
@@ -7,10 +7,15 @@ const _image 	= require('./imageCommands.js');
 const _text 	= require('./textCommands.js');
 const _youtube 	= require('./youtubeService.js');
 
+const fs = require('fs');
+
 const prefix = "a!";
 const interactionCooldown = 50;
 var   lastInteraction = 0;
-
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
 const client = new Discord.Client();
 client.on("message", function(message) {
 	if(message.author.bot) return;
@@ -33,10 +38,30 @@ client.on("message", function(message) {
 	switch(command){
 		case "help": message.reply(_generic.help(prefix)); break;
 		
-		case "textgura":	case "tg": 	message.channel.send(_text.gura()); break;
-		case "audiogura": 	case "ag": 	_audio.gura(message, args[0], prefix+command); break;
-		case "gurafacts":	case "gf": 	message.reply(_text.facts()); break;
+		case "textgura":	
+			case "tg": 	
+				var strText=_text.gura();
+				message.channel.send(strText); 
+				fs.appendFile('logs.txt',"["+dateTime+"] TG: " + strText +'\n', function (err) {
+					if (err) throw err;
+					console.log("["+dateTime+"] TG: " + strText);
+				});
+				break;
+
+		case "audiogura": case "ag": 	_audio.gura(message, args[0], prefix+command); break;
+		
+		case "gurafacts":	
+			case "gf":
+				var strText=_text.facts();
+				message.reply(strText); 
+				fs.appendFile('logs.txt',"["+dateTime+"] GF: " + strText +'\n', function (err) {
+					if (err) throw err;
+					console.log("["+dateTime+"] GF: " + strText);
+				}); 	
+				break;
+
 		case "gurapics": 	case "gp": 	message.channel.send({files: [{attachment: _image.gura(), size: 4096}]}); break;
+		//case "getstream": case "gs":
 		//case "test": 		_youtube.test(message); break;
 		
 		default: message.channel.send(_text.gura());
